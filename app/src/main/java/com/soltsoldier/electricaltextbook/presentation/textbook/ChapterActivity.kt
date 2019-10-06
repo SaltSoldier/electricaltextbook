2b0fcc1d72077a4e88755d698aeb659fdb72ca4c
Ryohei Shioji <rshioji@yahoo-corp.jp>
2019-10-06 15:53:16 +0900

    [Add] 章ボタンを押したら章ページに遷移

--- app/src/main/java/com/soltsoldier/electricaltextbook/presentation/textbook/ChapterActivity.kt
+++ app/src/main/java/com/soltsoldier/electricaltextbook/presentation/textbook/ChapterActivity.kt
@@ -5,9 +5,12 @@ import com.soltsoldier.electricaltextbook.R
 import com.soltsoldier.electricaltextbook.presentation.base.BaseActivity
 
 class ChapterActivity : BaseActivity() {
+    private var mChapterName: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chapter)
+        mChapterName = intent.getStringExtra("chapterName")
+        setTitle(mChapterName)
     }
 }
--- app/src/main/java/com/soltsoldier/electricaltextbook/presentation/top/TopRecyclerViewAdapter.kt
+++ app/src/main/java/com/soltsoldier/electricaltextbook/presentation/top/TopRecyclerViewAdapter.kt
@@ -1,12 +1,14 @@
 package com.soltsoldier.electricaltextbook.presentation.top
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.soltsoldier.electricaltextbook.R
+import com.soltsoldier.electricaltextbook.presentation.textbook.ChapterActivity
 
 class TopRecyclerViewAdapter(chapterList: List<String>, context: Context) :
     RecyclerView.Adapter<TopRecyclerViewAdapter.ViewHolder>() {
@@ -26,6 +28,12 @@ class TopRecyclerViewAdapter(chapterList: List<String>, context: Context) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.chapterName.text = mChapter.get(position)
+        holder.chapterName.setOnClickListener{
+            val intent = Intent(mContext, ChapterActivity::class.java)
+            // 章の名前だけ受け渡しておく（次のページのAppbarタイトルに設定）
+            intent.putExtra("chapterName", mChapter.get(position))
+            mContext.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = mChapter.size
