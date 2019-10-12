16893e4ad6d4f6c6f95c96a87b3ca9e6095c1850
Ryohei Shioji <rshioji@yahoo-corp.jp>
2019-10-12 17:59:24 +0900

    [Add] 検索文字列のクリックアクション追加

---
+++ app/src/main/java/com/soltsoldier/electricaltextbook/presentation/linkword/Chapter1Page1.kt
@@ -0,0 +1,9 @@
+package com.soltsoldier.electricaltextbook.presentation.linkword
+
+class Chapter1Page1() {
+    val linkWord = mapOf(
+            "電気" to 0,
+            "電荷" to 1,
+            "電流" to 2
+    )
+}
--- app/src/main/java/com/soltsoldier/electricaltextbook/presentation/page/PageFragment.kt
+++ app/src/main/java/com/soltsoldier/electricaltextbook/presentation/page/PageFragment.kt
@@ -1,11 +1,24 @@
 package com.soltsoldier.electricaltextbook.presentation.page
 
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.soltsoldier.electricaltextbook.R
 import com.soltsoldier.electricaltextbook.presentation.base.BaseFragment
+import com.soltsoldier.electricaltextbook.presentation.chapter.ChapterActivity
+import com.soltsoldier.electricaltextbook.presentation.linkword.Chapter1Page1
+import kotlinx.android.synthetic.main.fragment_chapter2_page1.view.*
+
 
 class PageFragment : BaseFragment() {
 
@@ -29,9 +42,9 @@ class PageFragment : BaseFragment() {
 
     // ここに章の追加、ページの追加を行なってください
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         var view: View? = null
         // チャプターで場合分け
@@ -46,7 +59,16 @@ class PageFragment : BaseFragment() {
             }
             1 -> {
                 when (pageNum) {
-                    0 -> view = inflater.inflate(R.layout.fragment_chapter2_page1, container, false)
+                    0 -> {
+                        view = inflater.inflate(R.layout.fragment_chapter2_page1, container, false)
+                        val setText1 = view!!.chapter2_page1_text1.text.toString()
+                        val setText2 = view!!.chapter2_page1_text2.text.toString()
+                        // リンクを貼りたい文章の数だけ実行してください
+                        view.chapter2_page1_text1.text = setLink(setText1)
+                        view.chapter2_page1_text1.movementMethod = LinkMovementMethod.getInstance()
+                        view.chapter2_page1_text2.text = setLink(setText2)
+                        view.chapter2_page1_text2.movementMethod = LinkMovementMethod.getInstance()
+                    }
                     1 -> view = inflater.inflate(R.layout.fragment_chapter2_page2, container, false)
                     2 -> view = inflater.inflate(R.layout.fragment_chapter2_page3, container, false)
                 }
@@ -64,5 +86,44 @@ class PageFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+
+    // 文字列にリンクをはる
+    fun setLink(baseText: String): SpannableStringBuilder {
+        val chapter1Page1 = Chapter1Page1()
+        val sb = SpannableStringBuilder()
+        sb.append(baseText)
+
+        // リンクを貼る文字のパターンだけ行う
+        for (linkWord in chapter1Page1.linkWord) {
+            var spanStart = 0
+            while (spanStart != -1) {
+                // 文字列検索
+                spanStart = baseText.indexOf(linkWord.key, spanStart)
+
+                // 文字列不一致の場合whileから脱出
+                if (spanStart == -1) break
+
+                // リンクを飛ばす文字列に加工を加える
+                // アンダーライン追記
+                sb.setSpan(UnderlineSpan(), spanStart, spanStart + linkWord.key.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                // 太字に
+                sb.setSpan(StyleSpan(Typeface.BOLD), spanStart, spanStart + linkWord.key.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                // クリックアクション追加
+                sb.setSpan(object : ClickableSpan() {
+                    override fun onClick(widget: View) {
+                        // クリックした時にページ遷移を組み込む
+                        val intent = Intent(view!!.context, ChapterActivity::class.java)
+                        // 章の名前と章の番号を渡しておく
+                        intent.putExtra("chapterName", linkWord.key + "とは")
+                        intent.putExtra("chapterNum", linkWord.value)
+                        view!!.context.startActivity(intent)
+                    }
+                }, spanStart, spanStart + linkWord.key.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                // 検索indexの更新
+                spanStart += linkWord.key.length
+            }
+        }
+        return sb
     }
 }
